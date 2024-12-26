ea9dbdb7fa1687c3ff451389f05b8fe636ea39f3
huangr0867 <rhuang53@gmail.com>
2024-12-26 15:21:36 -0700

    feat: temp disable blog on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,31 +1,37 @@
-import '../styles/navbar.css'
-import ThemeToggle from './ThemeToggle';
-import { useTheme } from './ThemeContext';
+import "../styles/navbar.css";
+import ThemeToggle from "./ThemeToggle";
+import { useTheme } from "./ThemeContext";
 
 const Navbar = () => {
-    const { theme, } = useTheme();
-    const handleThemeToggle = () => {
-        // You can implement logic to update your application's theme
-        console.log('Theme toggled to:', theme);
-    };
-    
-    return (
-        <nav className={`horizontal-navbar ${theme}`}>
-            <div className="center-nav">
-                <div className="nav-tabs">
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/research" className="new-page">Research</a></li>
-                    <li><a href="/blogs" className="new-page">Blog</a></li>
-                    {/* <li><a href="/experience">Experience</a></li> */}
-                </ul>
-                </div>
-                <div className="nav-buttons">
-                    <ThemeToggle onToggle={handleThemeToggle}/>
-                </div>
-            </div>
-        </nav>
-      );
-}
+  const { theme } = useTheme();
+  const handleThemeToggle = () => {
+    // You can implement logic to update your application's theme
+    console.log("Theme toggled to:", theme);
+  };
+
+  return (
+    <nav className={`horizontal-navbar ${theme}`}>
+      <div className="center-nav">
+        <div className="nav-tabs">
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/research" className="new-page">
+                Research
+              </a>
+            </li>
+            {/* <li><a href="/blogs" className="new-page">Blog</a></li> */}
+            {/* <li><a href="/experience">Experience</a></li> */}
+          </ul>
+        </div>
+        <div className="nav-buttons">
+          <ThemeToggle onToggle={handleThemeToggle} />
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
