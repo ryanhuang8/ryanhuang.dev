f4a7bd8020390b2d29ee2e9437f42af89713ba51
Ryan Huang <rhuang53@gmail.com>
2025-06-15 23:13:26 -0700

    feat: experience

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -1,10 +1,21 @@
 import { useTheme } from '../components/ThemeContext'
+import Resume from '../../src/files/Ryan Huang Resume 2025.pdf'
+import { IoDocumentText } from 'react-icons/io5';
 
 function Experience() {
   const { theme, } = useTheme();
+  // TODO: Fix experience page later, temporary code to add link to resume
   return (
     <div className={`container ${theme}`}>
-      Experience
+      <div style={{ marginTop: '60px' }}>
+        Experience
+        <a href={Resume} target="_blank">
+                  <div className="icon-text">
+                      <IoDocumentText size={24}/>
+                      <div className="text-by-icon">Resume</div>
+                  </div>
+        </a>
+      </div>
     </div>
   )
 }
